a9917f68535691e5f36811de625301fd1aa105d8
rudisiona <rsmageo@gmail.com>
2025-08-12 20:38:50 -0700

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import { useState } from 'react'
+import NavBar from './components/NavBar/NavBar';
+import MailboxDetails from './components/MailboxDetails/MailboxDetails';
+import MailboxForm from './components/MailboxForm/MailboxForm';
+import MailboxList from './components/MailboxList/MailboxList';
 import './App.css'
 
 function App() {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+    <NavBar />
+  <h1>Hello world!</h1>
+</>
+
+)
 };
 
 export default App;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import  {Link} from 'react-router'
+
+function NavBar () {
+    return (
+        <nav>
+        
+            <Link to="/">Home</Link>
+            <Link to="/mailboxes">Mailboxes</Link>
+            <Link to="/new-mailbox">New Mailbox</Link>
+        </nav>
+    )
+}
+
+export default NavBar;
