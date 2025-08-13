e5d1326d15cc5b9c89f0db1dac4cd1c594324673
rudisiona <rsmageo@gmail.com>
2025-08-12 20:51:21 -0700

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,22 @@ import NavBar from './components/NavBar/NavBar';
 import MailboxDetails from './components/MailboxDetails/MailboxDetails';
 import MailboxForm from './components/MailboxForm/MailboxForm';
 import MailboxList from './components/MailboxList/MailboxList';
+import { Route, Routes } from 'react-router'
 import './App.css'
 
 function App() {
   return (
     <>
+      <h1>Hello world!</h1>
     <NavBar />
-  <h1>Hello world!</h1>
+    <Routes>
+      <Route path="/" element={<main><h1>Post Office</h1></main>} />
+      <Route path="/mailboxes" element={<MailboxList />} />
+      <Route path="/new-mailbox" element={<MailboxForm />} />
+      <Route path="/mailboxes/:mailboxId" element={<MailboxDetails />} />
+
+    </Routes>
+
 </>
 
 )
