4be64c7957f6d102c2fdf6faeb756627119f47cc
rudisiona <rsmageo@gmail.com>
2025-08-17 18:54:50 -0700

    slight update

--- src/components/MailboxList/MailboxList.jsx
+++ src/components/MailboxList/MailboxList.jsx
@@ -1,29 +1,34 @@
 import React from 'react'
 import NavBar from '../NavBar/NavBar.jsx'
 import { Link } from 'react-router' 
+
+
+
 const MailboxList = ({mailboxes}) => {
-
   return (
-    
     <div>
-      
-      {mailboxes.map((mailbox, index) => {
-       
-       const boxStyle = {
-        border: '1px solid black'
-       }
-       return (
-       <div key={mailbox._id} style={boxStyle}>
-          <Link to={`/mailboxes/${mailbox._id}`}>
-          <h1>{mailbox.boxOwner}</h1>
-          </Link>
-          <p>Box Number: {mailbox._id}</p>
-
+      {mailboxes.length === 0 ? (
+        <p>There are currently no mailboxes.</p>
+      ) : (
+        mailboxes.map((mailbox) => (
+          <div
+            key={mailbox._id}
+            style={{
+              border: "1px solid black",
+              borderRadius: "8px",
+              padding: "1rem",
+              margin: "0.5rem 0",
+            }}
+          >
+            <Link to={`/mailboxes/${mailbox._id}`}>
+              <h1>{mailbox.boxOwner}</h1>
+            </Link>
+            <p>Box Number: {mailbox._id}</p>
           </div>
-      )})}
+        ))
+      )}
     </div>
-    
-  )
-}
+  );
+};
 
 export default MailboxList
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
 import  {Link} from 'react-router'
-
+import "./NavBar.css"
 function NavBar () {
     return (
         <nav>
